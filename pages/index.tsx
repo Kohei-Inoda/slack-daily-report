12eb26d326c6c1ed8251678fdf2b71031dae1318
Kohei-inoda <kohei277@gmail.com>
2025-06-05 00:15:51 +0900

    24時まで追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,7 +25,7 @@ export default function Home() {
 
   const generateTimeOptions = () => {
     const options = [];
-    for (let hour = 10; hour <= 19; hour++) {
+    for (let hour = 10; hour <= 24; hour++) {
       for (const min of [0, 30]) {
         if (hour === 19 && min > 0) continue;
         const hh = String(hour).padStart(2, "0");
